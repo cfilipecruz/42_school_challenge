ce12261eed1686ea58dd4798938f075ee285973c
Filipe Cruz <marques6832@gmail.com>
2026-04-27 15:34:19 +0000

    New file System

---
+++ piscine/C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarques <cmarques@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 22:41:32 by cmarques          #+#    #+#             */
+/*   Updated: 2026/02/19 14:02:06 by cmarques         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+	int	size;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	size = max - min;
+	*range = (int *)malloc(size * sizeof(int));
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (size);
+}
