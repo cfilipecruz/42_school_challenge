5889088acc09dd5de7350f3db4cad1b21e80b2d3
Filipe Cruz <marques6832@gmail.com>
2026-04-28 09:50:33 +0000

    update ft_strtrim

--- common _core/Milestone_01/libft/ft_strtrim.c
+++ common _core/Milestone_01/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cmarques <cmarques@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:50:12 by cmarques          #+#    #+#             */
-/*   Updated: 2026/04/27 22:24:26 by cmarques         ###   ########.fr       */
+/*   Updated: 2026/04/28 09:41:09 by cmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,41 @@
 
 int	ft_strlen(char *str);
 
+int checkchar(char c, char *set){
+    int i;
+    
+    i= 0;
+    while(set[i]){
+        if(set[i] == c)
+            return(1);
+        i++;
+    }
+    return(0);
+}
+
 char *ft_strtrim(char const *s1, char const *set){
     int i;
     int j;
     char *result;
-    int len1;
-    int len2;
+    int len;
     int c;
     
-    len1 = ft_strlen((char *)s1);
-    len2 = ft_strlen((char *)set);
+    len = ft_strlen((char *)s1);
     i = 0;
     j = 0;
     c = 0;
-    while()
+    
+    while(checkchar(s1[i], (char *)set))
         i++;
-    while(s1[len-i+j]==set[j] && set[j])
+    printf("%i\n", i);
+    printf("%i\n", len);
+    printf("%i\n", len - j);
+    while(checkchar(s1[len - j], (char *)set)){
+        printf("%i\n", len - j);
         j++;
+    }
+    printf("%i\n", j);
+    
     result = malloc((i+j)*sizeof(char));
     if(!result)
         return(NULL);
@@ -46,6 +64,6 @@ char *ft_strtrim(char const *s1, char const *set){
 int main(void){
     char *result;
 
-    result = ft_strtrim(" 7Teste 8 ", " 7 ");
+    result = ft_strtrim(" 78Teste 87", "87 ");
     printf("%s\n", result);
 }
